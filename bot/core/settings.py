f8723ccce0a13a25dd53af380de4a2f1397a67ae
”imcatboy” <danik.el757@gmail.com>
2026-05-24 01:28:43 +0300

    feat: Add proxy

--- bot/core/settings.py
+++ bot/core/settings.py
@@ -7,5 +7,6 @@ class Settings(BaseSettings):
     REDIS_HOST: str
     REDIS_PORT: int
     REDIS_DB: int
+    PROXY_URL: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.redis import RedisStorage
 from aiogram import Bot, Dispatcher
@@ -48,7 +49,7 @@ redis = Redis(
 )
 
 fsm_storage = RedisStorage(redis)
-bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML), session=AiohttpSession(proxy=settings.PROXY_URL))
 dp = Dispatcher(storage=fsm_storage)
 
 scheduler_service = SchedulerService(
