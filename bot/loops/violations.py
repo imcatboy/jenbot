90e9f75ac5bc7e296bef0a9cad07ad4530a44ee9
”imcatboy” <danik.el757@gmail.com>
2026-05-26 02:03:39 +0300

    fix: Modify actualize violations loop

--- bot/loops/violations.py
+++ bot/loops/violations.py
@@ -6,36 +6,65 @@ from datetime import datetime
 from aiogram import Bot
 from typing import List
 
-from domain.services import ModerationService
+from domain.services import ModerationService, ConfigService
 from domain.objects.types import ViolationType
+from domain.objects import entities
 
 
-async def actualize_violations_loop(bot: Bot, moderation_service: ModerationService):
+async def _check_warn_active(violation: entities.ChatViolationWithUserEntity) -> bool:
+    if not violation.expires_at or violation.expires_at > datetime.now():
+        return True
+
+    return False
+
+
+async def _check_status_active(
+    status: ChatMemberStatus,
+    violation: entities.ChatViolationWithUserEntity,
+    bot: Bot,
+    chat_ids: List[int],
+) -> bool:
+    check_chat_ids = (
+        [violation.telegram_chat_id] if violation.telegram_chat_id else chat_ids
+    )
+    has_active_status = False
+
+    for chat_id in check_chat_ids:
+        try:
+            member = await bot.get_chat_member(chat_id, violation.user.telegram_id)
+
+            if member.status == status:
+                has_active_status = True
+                break
+        except TelegramBadRequest:
+            continue
+
+    return has_active_status
+
+
+async def actualize_violations_loop(
+    bot: Bot, moderation_service: ModerationService, config_service: ConfigService
+):
     violations = await moderation_service.get_violations_to_actualize()
     inactive_violations_ids: List[int] = []
+    chat_ids: List[int] = await config_service.get("chats", [])
 
     for violation in violations:
         if violation.type == ViolationType.WARN:
-            if violation.expires_at and violation.expires_at < datetime.now():
+            if not await _check_warn_active(violation):
                 inactive_violations_ids.append(violation.id)
         elif violation.type == ViolationType.MUTE:
-            try:
-                member = await bot.get_chat_member(violation.telegram_chat_id, violation.user.telegram_id)
-
-                if member.status != ChatMemberStatus.RESTRICTED:
-                    inactive_violations_ids.append(violation.id)
-            except TelegramBadRequest:
+            if not await _check_status_active(
+                ChatMemberStatus.RESTRICTED, violation, bot, chat_ids
+            ):
                 inactive_violations_ids.append(violation.id)
         elif violation.type == ViolationType.BAN:
-            try:
-                member = await bot.get_chat_member(violation.telegram_chat_id, violation.user.telegram_id)
-
-                if member.status != ChatMemberStatus.KICKED:
-                    inactive_violations_ids.append(violation.id)
-            except TelegramBadRequest:
+            if not await _check_status_active(
+                ChatMemberStatus.KICKED, violation, bot, chat_ids
+            ):
                 inactive_violations_ids.append(violation.id)
-        
+
         await asyncio.sleep(1)
-    
+
     if inactive_violations_ids:
         await moderation_service.set_violations_active(inactive_violations_ids, False)
