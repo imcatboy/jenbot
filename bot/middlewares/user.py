3a7b93eedc1e2b91093de43b1ea6748659cd1a3a
”imcatboy” <danik.el757@gmail.com>
2026-06-29 03:13:23 +0300

    fix: hotfix

--- bot/middlewares/user.py
+++ bot/middlewares/user.py
@@ -35,9 +35,9 @@ class UserMiddleware(BaseMiddleware):
             and not event.reply_to_message.sender_chat
             and event.reply_to_message.from_user
             and not event.reply_to_message.from_user.is_bot
-            and (
+            and not (
                 event.reply_to_message.is_topic_message
-                and event.reply_to_message.message_id != event.message_thread_id
+                and event.reply_to_message.message_id == event.message_thread_id
             )
         ):
             reply_from_user = event.reply_to_message.from_user
